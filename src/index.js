1e15f812f545fbfa7c251933815aca9e6e16a375
Santiago <santylore4@gmail.com>
2018-11-30 17:47:28 -0300

    Uploading my page

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 
+//La PAGINA DEL SANTI PAPA
+
 ReactDOM.render(<App />, document.getElementById('root'))
